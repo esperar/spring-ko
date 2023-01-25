076576b4a1decda237a064eca045c437db0c6e7a
aesperer <piyrw9754@gmail.com>
2023-01-26 01:29:08 +0900

    :sparkles: maria db 마이그레이션
    maria db

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
