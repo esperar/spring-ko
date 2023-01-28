c01c4cea27725322e86f81bcf784fd213fd5b5b1
aesperer <piyrw9754@gmail.com>
2023-01-28 11:46:22 +0900

    :sparkles: RefreshToken 엔티티 생성
    RefreshToken

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/esperer/kopring/domain/refresh/domain/RefreshToken.kt
@@ -0,0 +1,15 @@
+package esperer.kopring.domain.refresh.domain
+
+import esperer.kopring.global.security.jwt.JwtTokenProvider
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
+
+@RedisHash(value = "refreshToken", timeToLive = JwtTokenProvider.REFRESH_EXP)
+class RefreshToken(
+    @Id
+    @Indexed
+    val userId: Long,
+    @Indexed
+    val token: String,
+)
