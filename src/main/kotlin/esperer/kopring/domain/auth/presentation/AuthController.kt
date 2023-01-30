e32a4b68e3d2918dc6157bf0937bbd596c544f6f
aesperer <piyrw9754@gmail.com>
2023-01-30 10:05:42 +0900

    :sparkles: 회원가입 api 구현
    회원가입

--- src/main/kotlin/esperer/kopring/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/esperer/kopring/domain/auth/presentation/AuthController.kt
@@ -1,9 +1,27 @@
 package esperer.kopring.domain.auth.presentation
 
+import esperer.kopring.domain.auth.presentation.data.request.SignUpRequest
+import esperer.kopring.domain.auth.service.MemberAuthService
+import esperer.kopring.domain.auth.util.AccountConverter
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/auth")
-class AuthController {
+class AuthController(
+    private val memberAuthService: MemberAuthService,
+    private val accountConverter: AccountConverter
+) {
+
+    @PostMapping
+    fun signUp(@RequestBody @Valid request: SignUpRequest) : ResponseEntity<Void> =
+        accountConverter.toDto(request)
+            .let { memberAuthService.signUp(it) }
+            .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
 }
