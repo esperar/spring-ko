7cf41c4c571379622d8b76cc36c90936c8995cf1
aesperer <piyrw9754@gmail.com>
2023-02-24 23:45:06 +0900

    :sparkles: 로그인 서비스
    로그인 서비스

--- src/main/kotlin/esperer/kopring/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/esperer/kopring/domain/auth/presentation/AuthController.kt
@@ -1,6 +1,9 @@
 package esperer.kopring.domain.auth.presentation
 
+import esperer.kopring.domain.auth.presentation.data.request.LoginRequest
 import esperer.kopring.domain.auth.presentation.data.request.SignUpRequest
+import esperer.kopring.domain.auth.presentation.data.response.TokenResponse
+import esperer.kopring.domain.auth.service.MemberLoginService
 import esperer.kopring.domain.auth.service.MemberSignUpService
 import esperer.kopring.domain.auth.util.AccountConverter
 import org.springframework.http.HttpStatus
@@ -15,6 +18,7 @@ import javax.validation.Valid
 @RequestMapping("/auth")
 class AuthController(
     private val memberSignUpService: MemberSignUpService,
+    private val memberLoginService: MemberLoginService,
     private val accountConverter: AccountConverter
 ) {
 
@@ -23,5 +27,11 @@ class AuthController(
         accountConverter.toDto(request)
             .let { memberSignUpService.execute(it) }
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @PostMapping("login")
+    fun login(@RequestBody @Valid request: LoginRequest): ResponseEntity<TokenResponse> =
+        accountConverter.toDto(request)
+            .let { memberLoginService.execute(it) }
+            .let { ResponseEntity.ok(it) }
 
 }
