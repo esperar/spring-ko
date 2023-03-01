7afec6fa490a6e82e008f2241dc3b3ea233a1f6c
aesperer <piyrw9754@gmail.com>
2023-03-01 16:19:55 +0900

    :sparkles: get post api

--- src/main/kotlin/esperer/kopring/domain/post/presentation/PostController.kt
+++ src/main/kotlin/esperer/kopring/domain/post/presentation/PostController.kt
@@ -2,8 +2,11 @@ package esperer.kopring.domain.post.presentation
 
 import esperer.kopring.domain.post.presentation.dto.request.CreatePostRequest
 import esperer.kopring.domain.post.service.CreatePostService
+import esperer.kopring.domain.post.service.GetPostService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,11 +17,20 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/post")
 class PostController(
-    private val createPostService: CreatePostService
+    private val createPostService: CreatePostService,
+    private val getPostService: GetPostService
 ) {
 
     @PostMapping
     fun createPost(@RequestBody @Valid createPostRequest: CreatePostRequest): ResponseEntity<Long> =
         createPostService.execute(createPostRequest)
             .let { ResponseEntity.status(HttpStatus.CREATED).body(it) }
+
+    @GetMapping("{id}")
+    fun getPostById(@PathVariable id: Long) =
+        getPostService.execute(id)
+            .let { ResponseEntity.ok(it) }
+
+
+
 }
