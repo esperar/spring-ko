5d648c48ee146ecffee39c246c3147d9774e8c60
aesperer <piyrw9754@gmail.com>
2023-03-01 23:13:46 +0900

    :sparkles: update post api

--- src/main/kotlin/esperer/kopring/domain/post/presentation/PostController.kt
+++ src/main/kotlin/esperer/kopring/domain/post/presentation/PostController.kt
@@ -1,14 +1,17 @@
 package esperer.kopring.domain.post.presentation
 
 import esperer.kopring.domain.post.presentation.dto.request.CreatePostRequest
+import esperer.kopring.domain.post.presentation.dto.request.UpdatePostRequest
 import esperer.kopring.domain.post.presentation.dto.response.PostDetailResponse
 import esperer.kopring.domain.post.presentation.dto.response.PostResponse
 import esperer.kopring.domain.post.service.CreatePostService
 import esperer.kopring.domain.post.service.GetAllPostService
 import esperer.kopring.domain.post.service.GetPostService
+import esperer.kopring.domain.post.service.UpdatePostService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -22,7 +25,8 @@ import javax.validation.Valid
 class PostController(
     private val createPostService: CreatePostService,
     private val getPostService: GetPostService,
-    private val getAllPostService: GetAllPostService
+    private val getAllPostService: GetAllPostService,
+    private val updatePostService: UpdatePostService
 ) {
 
     @PostMapping
@@ -30,7 +34,7 @@ class PostController(
         createPostService.execute(createPostRequest)
             .let { ResponseEntity.status(HttpStatus.CREATED).body(it) }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     fun getPostById(@PathVariable id: Long): ResponseEntity<PostDetailResponse> =
         getPostService.execute(id)
             .let { ResponseEntity.ok(it) }
@@ -39,5 +43,11 @@ class PostController(
     fun getAllPost(): ResponseEntity<List<PostResponse>> =
         getAllPostService.execute()
             .let { ResponseEntity.ok(it) }
+
+    @PatchMapping("/{id}")
+    fun updatePost(@PathVariable id: Long, updatePostRequest: UpdatePostRequest): ResponseEntity<Void> =
+        updatePostService.execute(id, updatePostRequest)
+            .let { ResponseEntity.noContent().build() }
+
 
 }
