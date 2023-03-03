577ad2827121fd54696d90298f6b5bc3dbbfd2df
aesperer <piyrw9754@gmail.com>
2023-03-04 01:44:16 +0900

    :sparkles: update  comment business

--- src/main/kotlin/esperer/kopring/domain/comment/service/UpdateCommentService.kt
+++ src/main/kotlin/esperer/kopring/domain/comment/service/UpdateCommentService.kt
@@ -1,19 +1,20 @@
 package esperer.kopring.domain.comment.service
 
+import esperer.kopring.domain.auth.util.MemberUtil
 import esperer.kopring.domain.comment.domain.repository.CommentRepository
 import esperer.kopring.domain.comment.exception.CommentNotFoundException
 import esperer.kopring.domain.comment.presentation.dto.request.UpdateCommentRequest
 import esperer.kopring.global.annotation.TransactionalService
-import org.springframework.data.repository.findByIdOrNull
 
 @TransactionalService
 class UpdateCommentService(
-    private val commentRepository: CommentRepository
+    private val commentRepository: CommentRepository,
+    private val memberUtil: MemberUtil
 ) {
 
     fun execute(commentId: Long, updateCommentRequest: UpdateCommentRequest) =
-        commentRepository.findByIdOrNull(commentId)
-            .let { it ?: CommentNotFoundException() }
-            .let {  }
+        commentRepository.findByIdAndMember(commentId, memberUtil.currentMember())
+            .let { it ?: throw CommentNotFoundException() }
+            .let { commentRepository.save(it.update(updateCommentRequest)) }
 
 }
