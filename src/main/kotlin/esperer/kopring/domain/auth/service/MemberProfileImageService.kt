b7eb2a846e5bd07bacf25a6e3655f81954c22976
aesperer <piyrw9754@gmail.com>
2023-03-20 13:06:09 +0900

    :recycle: save update object

--- src/main/kotlin/esperer/kopring/domain/auth/service/MemberProfileImageService.kt
+++ src/main/kotlin/esperer/kopring/domain/auth/service/MemberProfileImageService.kt
@@ -3,5 +3,5 @@ package esperer.kopring.domain.auth.service
 import esperer.kopring.domain.auth.presentation.data.dto.MemberDto
 
 interface MemberProfileImageService {
-    fun execute(userDto: MemberDto)
+    fun execute(memberDto: MemberDto)
 }
--- src/main/kotlin/esperer/kopring/domain/auth/service/impl/MemberProfileImageServiceImpl.kt
+++ src/main/kotlin/esperer/kopring/domain/auth/service/impl/MemberProfileImageServiceImpl.kt
@@ -4,6 +4,7 @@ import esperer.kopring.domain.auth.presentation.data.dto.MemberDto
 import esperer.kopring.domain.auth.service.MemberProfileImageService
 import esperer.kopring.domain.auth.util.MemberConverter
 import esperer.kopring.domain.auth.util.MemberUtil
+import esperer.kopring.domain.member.domain.repo.MemberRepository
 import esperer.kopring.global.annotation.TransactionalService
 import esperer.kopring.infra.s3.service.S3Service
 
@@ -11,9 +12,16 @@ import esperer.kopring.infra.s3.service.S3Service
 class MemberProfileImageServiceImpl(
     private val memberUtil: MemberUtil,
     private val memberConverter: MemberConverter,
-    private val s3Service: S3Service
+    private val s3Service: S3Service,
+    private val memberRepository: MemberRepository
 ) : MemberProfileImageService {
-    override fun execute(userDto: MemberDto) {
-        TODO("Not yet implemented")
+
+    override fun execute(memberDto: MemberDto) {
+        val uploadFile = memberDto.file?.let { s3Service.uploadFile(it, "USER/") }
+        memberUtil.currentMember()
+            .let {
+                if(uploadFile != null) memberRepository.save(it.updateMemberProfileImage(uploadFile))
+                else memberRepository.save(it.updateMemberProfileImage(it.imageUrl))
+            }
     }
 }
