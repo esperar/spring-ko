359edfbf5cc1ec1459be778cba29df085d60f77f
aesperer <piyrw9754@gmail.com>
2023-03-20 13:21:21 +0900

    :art: aws yml

--- src/main/kotlin/esperer/kopring/domain/member/presentation/MemberController.kt
+++ src/main/kotlin/esperer/kopring/domain/member/presentation/MemberController.kt
@@ -5,9 +5,7 @@ import esperer.kopring.domain.member.service.MemberProfileImageService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
