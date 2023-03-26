918aa5bfe38c8e5773b0166880ce3a80951d2a5b
aesperer <piyrw9754@gmail.com>
2023-03-26 12:45:15 +0900

    :art: yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
 	implementation ("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
-
-
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/esperer/kopring/domain/refresh/domain/repo/RefreshTokenRepository.kt
+++ src/main/kotlin/esperer/kopring/domain/refresh/domain/repo/RefreshTokenRepository.kt
@@ -2,6 +2,7 @@ package esperer.kopring.domain.refresh.domain.repo
 
 import esperer.kopring.domain.refresh.domain.RefreshToken
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 interface RefreshTokenRepository: JpaRepository<RefreshToken, String> {
 }
